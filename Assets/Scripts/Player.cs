d558b2dd958e933052b3c9bd78aad5c05507a36c
Frightshade11g <frightshade116@gmail.com>
2024-11-29 11:57:48 -0800

    Grounded not working but otherwise fine.
    Grounded not working but otherwise fine.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,12 @@ public class Player : MonoBehaviour
     private Player_Base playerBase;
     private Rigidbody2D rb;
     private Collider2D col;
+    private BoxCollider2D boxCol;
 
     [Header("Jumping Variables")]
-    bool isGrounded;
-    [SerializeField] float juniorJumpVelocity = 10f;
+    [SerializeField] float jumpVelocity = 1000f;
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] float lowJumpMultiplier = 2.5f;
 
     [SerializeField] private LayerMask groundlayerMask;
 
@@ -19,29 +21,38 @@ public class Player : MonoBehaviour
     {
         playerBase = GetComponent<Player_Base>();
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<Collider2D>();
+        //col = GetComponent<Collider2D>();
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         Jump();
+        //GravityControl();
     }
 
     #region Jumping
 
     private void Jump()
     {
-        if (InputManager.JumpWasPressed && IsGrounded())
+        if (IsGrounded() && InputManager.JumpWasPressed)
         {
-            rb.velocity = Vector2.up * juniorJumpVelocity;
+            rb.velocity = Vector2.up * jumpVelocity;
         }
     }
 
+    private void GravityControl()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime) * Vector2.up;
+        }
+    }
     private bool IsGrounded()
     {
-        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down * 0.1f, groundlayerMask);
-        Debug.Log(rayCastHit2D.collider);
-        return rayCastHit2D.collider != null;
+        RaycastHit2D groundedRayCastHit2D = Physics2D.BoxCast(boxCol.bounds.center, boxCol.bounds.size, 0f, 0.1f * Vector2.down, groundlayerMask);
+        Debug.Log(groundedRayCastHit2D.collider);
+        return groundedRayCastHit2D.collider != null;
     }
 
     #endregion
