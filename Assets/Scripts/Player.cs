86544ab788895b3c23927c31c42f786b1746ee77
Frightshade11g <frightshade116@gmail.com>
2024-11-29 21:49:13 -0800

    JUMPING COMPLETE
    Just needs tuning (Later)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,40 +7,50 @@ public class Player : MonoBehaviour
     [Header("References")]
     private Player_Base playerBase;
     private Rigidbody2D rb;
-    private Collider2D col;
+    private CapsuleCollider2D col;
+    //private BoxCollider2D boxCol;
 
     [Header("Jumping Variables")]
-    bool isGrounded;
-    [SerializeField] float juniorJumpVelocity = 10f;
-
+    [SerializeField] float jumpVelocity = 10f;
+    [SerializeField] float fallMultiplier = 11f;
+    [SerializeField] float lowJumpMultiplier = 11f;
     [SerializeField] private LayerMask groundlayerMask;
 
     void Awake()
     {
         playerBase = GetComponent<Player_Base>();
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<Collider2D>();
+        col = GetComponent<CapsuleCollider2D>();
+        //boxCol = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         Jump();
+        GravityControls();
     }
 
     #region Jumping
 
     private void Jump()
     {
-        if (InputManager.JumpWasPressed && IsGrounded())
+        if (InputManager.JumpIsHeld && IsGrounded())
         {
-            rb.velocity = Vector2.up * juniorJumpVelocity;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private void GravityControls()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime) * Vector2.up;
         }
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down * 0.1f, groundlayerMask);
-        Debug.Log(rayCastHit2D.collider);
+        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, 0.1f, groundlayerMask);
         return rayCastHit2D.collider != null;
     }
 
