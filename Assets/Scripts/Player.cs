24e679ceeb81785078e22eb2d2d2529737a5c0c0
Frightshade11g <frightshade116@gmail.com>
2024-12-11 11:55:36 -0800

    Jump Buffering Implemented
    Apparently, we don't like Addforce for jumping cause it causes issues. We just directly set the velocity to fix all jumping errors.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("References")]
-    private Player_Base playerBase;
     private Rigidbody2D rb;
     private CapsuleCollider2D col;
-    //private BoxCollider2D boxCol;
 
     [Header("Jumping Variables")]
     [SerializeField] float jumpVelocity = 10f;
-    [SerializeField] float fallMultiplier = 11f;
-    //[SerializeField] float lowJumpMultiplier = 11f;
     [SerializeField] private LayerMask groundlayerMask;
     private float fallGravityScale = 6f;
 
     [Header("Variable Jump Height")]
     private bool jumping;
     private float buttonPressedTime;
-    float jumpCutGravityMultiplier = 12;
+    private float jumpCutGravityMultiplier = 12;
     [SerializeField] float buttonPressWindow;
     private float gravityScale = 4f;
+
+    [Header("Coyote Time/Jump Buffering")]
+    [SerializeField] private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+    private bool jumpBuffered = false;
+
+    [SerializeField] private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
 
     [Header("MovementVariables")]
     Vector2 moveVelocity;
@@ -30,60 +35,85 @@ public class Player : MonoBehaviour
 
     void Awake()
     {
-        playerBase = GetComponent<Player_Base>();
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
+        JumpBuffer();
+        CoyoteTimeChecker();
         Jump();
-        //GravityControls();
         HandleMovement();
         Animations();
     }
 
     #region Jumping
 
-    private void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            rb.gravityScale = gravityScale;
-            rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
-            jumping = true;
-            buttonPressedTime = 0;
-        }
-
-        if(jumping)
-        {
-            buttonPressedTime += Time.deltaTime;
-
-            if(buttonPressedTime < buttonPressWindow && Input.GetKeyUp(KeyCode.Space))
-            {
-                rb.gravityScale = jumpCutGravityMultiplier;
-            }
-            if(rb.velocity.y < 0)
-            {
-                rb.gravityScale = fallGravityScale;
-                jumping = false;
-            }
-        }
-    }
-
-    //private void GravityControls()
-    //{
-    //    if(rb.velocity.y < 0)
-    //    {
-    //        rb.velocity += (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime) * Vector2.up;
-    //    }
-    //}
-
     private bool IsGrounded()
     {
         RaycastHit2D rayCastHit2D = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, 0.1f, groundlayerMask);
-        Debug.Log("WORK");
         return rayCastHit2D.collider != null;
+    }
+
+    private void CoyoteTimeChecker()
+    {
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+            jumpBuffered = false;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    private void JumpBuffer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+            if (!IsGrounded())
+            {
+                jumpBuffered = true;
+            }
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime; ;
+        }
+    }
+
+    private void Jump()
+    {
+        if (!jumpBuffered)
+        {
+            if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
+            {
+                jumpBufferCounter = 0f;
+                rb.gravityScale = gravityScale;
+                rb.velocity = Vector2.up * jumpVelocity;
+                jumping = true;
+                buttonPressedTime = 0f;
+                coyoteTimeCounter = 0f;
+            }
+
+            if (jumping)
+            {
+                buttonPressedTime += Time.deltaTime;
+
+                if (buttonPressedTime < buttonPressWindow && Input.GetKeyUp(KeyCode.Space))
+                {
+                    rb.gravityScale = jumpCutGravityMultiplier;
+                }
+                if (rb.velocity.y < 0)
+                {
+                    rb.gravityScale = fallGravityScale;
+                    jumping = false;
+                }
+            }
+        }
     }
 
     #endregion
