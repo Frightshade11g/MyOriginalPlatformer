d358a2c88382f8b7cd2f42103d12d12c2d4bd862
Frightshade11g <frightshade116@gmail.com>
2025-01-06 11:57:20 -0800

    Bouncy Platforms Finished

---
+++ Assets/Scripts/BouncyPlatform.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPlatform : MonoBehaviour
+{
+    [SerializeField] private float bounce = 20f;
+    private bool bounced;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!bounced)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+                bounced = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (bounced)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                bounced = false;
+            }
+        }
+    }
+
+}
