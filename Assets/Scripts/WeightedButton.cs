cb6e7afff7c41d4d24402a8702464cc4e06da113
Frightshade11g <frightshade116@gmail.com>
2025-01-08 12:00:20 -0800

    Update, my button sucks

--- Assets/Scripts/WeightedButton.cs
+++ Assets/Scripts/WeightedButton.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class WeightedButton : MonoBehaviour
 {
     public GameObject thisObject;
     Rigidbody2D rbb;
     public BoxCollider2D boxcol;
+    [SerializeField] GameObject door;
 
     private void Awake()
     {
@@ -17,9 +19,12 @@ public class WeightedButton : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider == boxcol && collision.gameObject.CompareTag("Player"))
+        
+        if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Button Pressed");
             rbb.gravityScale = 1f;
+            door.SetActive(false);
         }
     }
 }
