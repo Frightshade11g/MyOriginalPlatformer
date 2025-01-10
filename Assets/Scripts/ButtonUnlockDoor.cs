e6c4e4b45f5cf453368015135d0756a0dc376d5a
Frightshade11g <frightshade116@gmail.com>
2025-01-10 12:00:37 -0800

    Interfaces
    Spent my time learning about interfaces because CodeMonkey's Tutorial uses them for the door opening tutorial. I want to be able to debug if anything goes wrong and I want to expand my programming comprehension.

---
+++ Assets/Scripts/ButtonUnlockDoor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonUnlockDoor : MonoBehaviour
+{
+    [SerializeField] private DoorSetActive doorSetActive;
+    void OnTriggerEnter2D()
+    {
+
+    }
+}
