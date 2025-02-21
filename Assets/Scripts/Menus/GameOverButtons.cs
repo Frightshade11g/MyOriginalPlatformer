b0491ab86d9550c450c09c9e0e7f422e012d4e16
Frightshade11g <frightshade116@gmail.com>
2025-02-21 12:15:21 -0800

    Game Over Screen
    I've completely finished the game over screen fit with button functionality, fading between scenes, and different TMPro font assets. Figuring out how to make a new font asset to avoid TMPro outlines affecting all the text took embarrassingly long.

---
+++ Assets/Scripts/Menus/GameOverButtons.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverButtons : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup myUIGroup;
+
+    [SerializeField] Button button;
+    [SerializeField] Button Otherbutton;
+
+    private void Start()
+    {
+        button.interactable = false;
+        Otherbutton.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (myUIGroup.alpha == 1)
+        {
+            button.interactable = true;
+            Otherbutton.interactable = true;
+        }
+    }
+
+    public void PlayGame()
+    {
+        if (myUIGroup.alpha == 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void QuitGame()
+    {
+        if (myUIGroup.alpha == 1)
+        {
+            Application.Quit();
+        }
+    }
+}
